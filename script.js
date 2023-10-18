a3c47651efa01d0de4b29022328cb64eee385eff
J George Avinash <145418454+george-avinash@users.noreply.github.com>
2023-10-18 17:29:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,17 @@ res.then((data) => data.json()).then((data1) => {
   var countryList = document.createElement('div');
   countryList.id = 'country-list';
   
-  // Create a Bootstrap container for the cards
   var container = document.createElement('div');
   container.className = 'container';
 
-  // Create a Bootstrap row for the columns
   var row = document.createElement('div');
   row.className = 'row row-cols-4';
 
   for (let i = 0; i < data1.length; i++) {
-    // Create a Bootstrap column for each card
+ 
     var col = document.createElement('div');
     col.className = 'col';
     
-    // Create a card with country information
     col.innerHTML = `
     <div class="card" style="width: 18rem;">
     <h5 class="card-title">${data1[i].name}</h5>
@@ -32,27 +29,22 @@ res.then((data) => data.json()).then((data1) => {
     </div>
     </div>`;
 
-    // Append the column to the row
+    
     row.appendChild(col);
   }
-
-  // Append the row to the container
   container.appendChild(row);
 
-  // Append the container to the country list
   countryList.appendChild(container);
-
-  // Append the country list to the document's body
+  
   document.body.appendChild(countryList);
 });
 function getWeather(countryName) {
-    // Use the OpenWeatherMap API here to get weather data for the selected country
-    // Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
+  
     var apiKey = 'e79844b7d5877f709b2f6eb80d96bc88';
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${countryName}&appid=${apiKey}`)
       .then((data) => data.json())
       .then((data) => {
-        // Handle the weather data and display it in your preferred format
+      
         alert(`Weather in ${countryName}: ${data.weather[0].description}`);
       })
       .catch((error) => {
